3bcf320f5bb28a4d7cba7551c8739a34c92174d4
Jorge Nanarro <jorgenavb@gmail.com>
2022-12-08 14:49:01 +0100

    Fix CI

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { ClickCounter2 } from './ClickCounter copy/ClickCounter';
 import { ClickCounter } from './ClickCounter/ClickCounter';
 
 import './index.scss';
 
 export default {
   'click-counter': ClickCounter,
+  'click-counter-2': ClickCounter2,
 } as Record<string, React.FC>;

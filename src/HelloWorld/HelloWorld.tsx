8c42472dc1e46bb49c364879d92359c28bb3d055
Jorge Navarro <40125551+jnavb@users.noreply.github.com>
2022-12-09 12:41:55 +0100

    Change hello component

--- src/HelloWorld/HelloWorld.tsx
+++ src/HelloWorld/HelloWorld.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { FormComponentProps } from 'src/formTypes';
+import React, { useState } from "react";
+import { FormComponentProps } from "src/formTypes";
 
 export const HelloWorld = ({
   config,
   payload,
   onChange,
 }: FormComponentProps) => {
-  return <div>Hello World!</div>;
+  return <div>Hello World :)</div>;
 };
